311fc6c1bea5223f4888ed587506bdc21f5db779
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-10 15:47:38 +0530

    Added constants file and stored as the constant variables in that file

--- FlashyChatApp/Controller/RegisterViewController.swift
+++ FlashyChatApp/Controller/RegisterViewController.swift
@@ -21,7 +21,7 @@ class RegisterViewController: UIViewController {
                     print(errorValue.localizedDescription)
                 }
                 else{
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    self.performSegue(withIdentifier: Constants.registerSegue, sender: self)
                 }
             }
         }
