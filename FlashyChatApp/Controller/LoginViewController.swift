f9dcbd6d44d307d450b6a6ebc8fb56d0a1c7c3a9
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-10 15:01:20 +0530

    Added code for logging into application and updated chat view controller to show navigation bars

--- FlashyChatApp/Controller/LoginViewController.swift
+++ FlashyChatApp/Controller/LoginViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                // removed [wek self] in completion handler, as the current code also works even without that
+                //  guard let strongSelf = self else { return }
+                if let errorValue = error{
+                    print(errorValue.localizedDescription)
+                }else{
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
--- FlashyChatApp/Controller/RegisterViewController.swift
+++ FlashyChatApp/Controller/RegisterViewController.swift
@@ -15,21 +15,14 @@ class RegisterViewController: UIViewController {
     
     @IBAction func registerPressed(_ sender: UIButton) {
         
-        // return type of .text is Optional(String), so initially unwrapping them safely via optional chaining
-        // adding on we added a comma seperator in if condition stating that if email is not nil.. assign it to email variable and same thing for password and only if both of these conditions don't fail move ahead with if block else no
         if let email = emailTextfield.text, let password = passwordTextfield.text{
-            // below line basically uses authentication module of firebase and creates a new user based on email and password details
-            // authResult is the users details after successful registration
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let errorValue = error{
-                    // localizedDescription method is used to log the error in readable format based on language of app
                     print(errorValue.localizedDescription)
                 }
                 else{
-                    // Navigate to chat view controller
                     self.performSegue(withIdentifier: "RegisterToChat", sender: self)
                 }
-                
             }
         }
     }
